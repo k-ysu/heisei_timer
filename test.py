69aa50528c4f5e31c3e6aa97387dea7b1d9ba5a0
yasushi <yaushikomori@yaushis-mbp.attlocal.net>
2018-04-29 21:44:56 -0700

    add test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+import tweepy, time, sys
+import random
+
+#argfile = str(sys.argv[1])
+
+
+#enter the corresponding information from your Twitter application:
+#CONSUMER_KEY = 'Rf3sthaNoNC5PN721AMChAtTv'#keep the quotes, replace this with your consumer key
+#CONSUMER_SECRET = 'FvlpyeR17R7diPslg2Z3SP6cyl1YWo3WmY0CfRolkhshzElu12' #keep the quotes, replace this with your consumer secret key
+#ACCESS_KEY = '973022013919916032-JGRr9FXWoB6awc5ATWUC44hwFn5f9O2'#keep the quotes, replace this with your access token
+#ACCESS_SECRET = 'vzeOrfPBcRNB7d6mVyujsSHU69TqUDb3NddfgNtxWFIzF'#keep the quotes, replace thbis with your access token secret
+#auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+#auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+#api = tweepy.API(auth)
+
+
+
+
+
+#filename=open(argfile,'r')
+#f=filename.readlines()
+#filename.close()
+
+#lines = open(argfile,'r').read().splitlines()
+#myline =random.choice(lines)
+#tweet = myline + "  " + random.choice(tags)+ "  " + random.choice(tags)
+#print(tweet)
+#api.update_status(tweet)
+
+
+#for line in f:
+#    tweet=line+str(random.randrange(1000))
+#    print (tweet)
+#    print line
+#    api.update_status(tweet)
+#    print ("done")
+#    time.sleep(180)#Tweet every 15 minutes
