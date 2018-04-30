1f7a8fd8dcc2d4d62bf8c235f0a2c29a7a79a9f7
yasushi <yaushikomori@yaushis-mbp.attlocal.net>
2018-04-30 00:07:08 -0700

    modify

--- test.py
+++ test.py
@@ -4,6 +4,27 @@ from datetime import date
 
 #argfile = str(sys.argv[1])
 
+#特定日付コメント
+specific_comment = {
+  "20180503": "憲法記念日",
+  "20180504": "みどりの日",
+  "20180505": "こどもの日",
+  "20180506": "ゴムの日",
+  "20180509": "アイスクリームの日",
+  "20180515": "沖縄復帰記念日",
+  "20180527": "百人一首の日",
+  "20180528": "花火の日",
+  "20180716": "海の日",
+  "20180811": "山の日",
+  "20180815": "終戦記念日",
+  "20180917": "敬老の日",
+  "20180923": "秋分の日",
+  "20181008": "体育の日",
+  "20181103": "文化の日",
+  "20181123": "勤労感謝の日",
+  "20181223": "天皇誕生日",
+}
+
 
 #enter the corresponding information from your Twitter application:
 CONSUMER_KEY = 'KVBoxCZQdUEqgeVxIgFMQcYku'#keep the quotes, replace this with your consumer key
@@ -16,22 +37,28 @@ api = tweepy.API(auth)
 
 
 # End of Heisei
-end_date = date(2019, 4, 30)
+end_date = date(2019, 5, 1)
 today = today = date.today()
 time_to_end = abs(end_date - today)
 to_heisei = time_to_end.days
-#to_heisei = 320
 
 #Progress
-
 progress = round ( (365-to_heisei) / 365 * 100)
 number_of_thin = round( 24 * to_heisei / 365 )
 number_of_thick = 24 - number_of_thin
 progress_bar = "▓" * number_of_thick
 progress_bar += "░" * number_of_thin
 
+#specific_comment
+key = "%04d%02d%02d" % ( today.year, today.month, today.day)
+special_comment = ""
+if ( key in specific_comment ):
+    special_comment += "今日は平成最後の{}月{}日({})です。\n".format(today.month, today.day , specific_comment[key])
+else:
+    special_comment += "今日は平成最後の{}月{}日です。\n".format(today.month, today.day)
 
-tweet = "今日は平成最後の{}月{}日です \n".format(today.month, today.day)
+
+tweet = special_comment
 tweet += "平成はあと" + str( time_to_end.days ) + "日です! \n"
 
 tweet += "{} {}%".format(progress_bar, progress)
